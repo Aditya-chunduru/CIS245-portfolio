0b35a5747fb63946e75876f7aa5e67a07097a19a
Aditya Chunduru <114924966+Definitive-edition@users.noreply.github.com>
2024-02-12 09:30:33 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function buttonreveal() {
    document.getElementById("About_me").innerHTML = "Hello, my name is Aditya chunduru"
 }
+
+function buttonrevealback() {
+   document.getElementById("About_me").innerHTML = "Who am i?"
+}
+
+function details() {
+   document.getElementById("Aditya").innerHTML = "I am an NECC IT Major and I've always been interested in technology. I use technology everyday to the point where I cannot go even a day without using technology in any form."
+}
+
+function colorchange() {
+   document.getElementById("goals").style.color='red'
+}
+function colorchangeback() {
+   document.getElementById("goals").style.color='black'
+}
